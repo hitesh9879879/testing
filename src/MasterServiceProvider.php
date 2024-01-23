3867c1a15f1e1cefef47975fb651e0e5f00a4782
Hitesh Suthar <hitesh.s@samcomtechnobrains.com>
2024-01-23 18:31:41 +0530

    model

--- src/MasterServiceProvider.php
+++ src/MasterServiceProvider.php
@@ -16,5 +16,10 @@ class MasterServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
         $this->loadViewsFrom(__DIR__ ."/resources/view/","Learning\Pkg");
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/');
+        $this->publishes([__DIR__.'/../database/migrations/' => database_path('migrations/')], 'migrations');
+
+        include __DIR__.'/Models/testing.php';
     }
 }
---
+++ src/Models/testing.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Learning\Pkg;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class testing extends Model
+{
+    use HasFactory;
+}
